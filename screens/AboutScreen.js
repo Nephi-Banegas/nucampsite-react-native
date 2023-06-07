50a628fdf882fec9dad98d7013aecd36e61102a2
Nephi Banegas <nephi.banegas@yahoo.com>
2023-06-06 19:53:06 -0500

    using redux

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,27 +1,31 @@
 import { View, Text } from "react-native-animatable";
 import { ScrollView } from "react-native";
 import { Avatar, Card, ListItem } from "react-native-elements";
-import { PARTNERS } from "../shared/partners";
-import { useState } from "react";
+import { useSelector } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
+// import { PARTNERS } from "../shared/partners";
+// import { useState } from "react";
 
 const Mission = () => {
-  <Card>
-    <Card.Title>Our Mission</Card.Title>
-    <Card.Divider />
-    <Text style={{ margin: 10 }}>
-      We present a curated database of the best campsites in the vast woods and
-      backcountry of the World Wide Web Wilderness. We increase access to
-      adventure for the public while promoting safe and respectful use of
-      resources. The expert wilderness trekkers on our staff personally verify
-      each campsite to make sure that they are up to our standards. We also
-      present a platform for campers to share reviews on campsites they have
-      visited with each other.
-    </Text>
-  </Card>;
+  return (
+    <Card>
+      <Card.Title>Our Mission</Card.Title>
+      <Card.Divider />
+      <Text style={{ margin: 10 }}>
+        We present a curated database of the best campsites in the vast woods
+        and backcountry of the World Wide Web Wilderness. We increase access to
+        adventure for the public while promoting safe and respectful use of
+        resources. The expert wilderness trekkers on our staff personally verify
+        each campsite to make sure that they are up to our standards. We also
+        present a platform for campers to share reviews on campsites they have
+        visited with each other.
+      </Text>
+    </Card>
+  );
 };
 
 const AboutScreen = () => {
-  const [partners, setPartners] = useState(PARTNERS);
+  const partners = useSelector((state) => state.partners);
 
   return (
     <ScrollView>
@@ -29,10 +33,10 @@ const AboutScreen = () => {
       <Card>
         <Card.Title>Community Partners</Card.Title>
         <Card.Divider />
-        {PARTNERS.map((partner) => {
+        {partners.partnersArray.map((partner) => {
           return (
             <ListItem key={partner.id}>
-              <Avatar source={partner.image} rounded />
+              <Avatar source={{ uri: baseUrl + partner.image }} rounded />
               <ListItem.Content>
                 <ListItem.Title>{partner.name}</ListItem.Title>
                 <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
